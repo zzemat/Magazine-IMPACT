27f9c31597f41b8230b44992c91dfaf901d30daf
Ilyass-Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-02-22 15:54:26 +0100

    Added Hashing the password

--- server/src/main/java/magazin/server/service/serviceImpl/UserServiceImpl.java
+++ server/src/main/java/magazin/server/service/serviceImpl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import magazin.server.repository.ProfileRepository;
 import magazin.server.repository.UserRepository;
 import magazin.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -20,8 +21,13 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private ProfileRepository profileRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Override
     public User createUser(User user) {
+        // TODO: make this better
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
