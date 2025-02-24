c2d86dfc07512fdd1b9e943732130f7150531021
Oubeza Idir <oubezaidir@gmail.com>
2025-02-24 02:42:28 +0000

    Add signup page and update login link to point to signup

--- magazine/src/app/login/page.jsx
+++ magazine/src/app/login/page.jsx
@@ -45,7 +45,7 @@ export default function Login() {
               Se connecter
             </button>
             <p className="text-center mt-4 text-gray-700">
-              vous n’avez pas un compte? <Link href="/login" className="font-bold hover:underline">s’inscrire</Link>
+              vous n’avez pas un compte? <Link href="/signup" className="font-bold hover:underline">s’inscrire</Link>
             </p>
           </div>
     </div>
