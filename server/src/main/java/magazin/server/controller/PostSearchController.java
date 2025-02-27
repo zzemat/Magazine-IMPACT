822f004e24fb2ccc0917ef5085487f5841a2058d
Ilyass Bougati <110248648+Ilyass-Bougati@users.noreply.github.com>
2025-02-27 00:32:19 +0000

    Added filtering by tags (for now)

--- server/src/main/java/magazin/server/controller/PostSearchController.java
+++ server/src/main/java/magazin/server/controller/PostSearchController.java
@@ -2,13 +2,13 @@ package magazin.server.controller;
 
 import magazin.server.entity.Post;
 import magazin.server.repository.PostRepository;
+import magazin.server.requests.PostSearchRequest;
 import magazin.server.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -19,9 +19,15 @@ public class PostSearchController {
     @Autowired
     PostRepository postRepository;
 
-    @PostMapping("/{title}")
-    public ResponseEntity<List<Post>> search(@PathVariable String title) {
-        Optional<List<Post>> posts = postRepository.findAllByTitle(title);
+    @PostMapping("/")
+    public ResponseEntity<List<Post>> search(@RequestBody PostSearchRequest request) {
+        // note : this probably can be refactored in the future
+        Optional<List<Post>> posts;
+        if (request.getTags() == null || request.getTags().isEmpty()) {
+            posts = postRepository.findAllByTitle(request.getTitle());
+        } else {
+            posts = postRepository.findAllByTitleAndTags(request.getTitle(), request.getTags());
+        }
         return posts.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
--- server/src/main/java/magazin/server/repository/PostRepository.java
+++ server/src/main/java/magazin/server/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package magazin.server.repository;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import magazin.server.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +12,11 @@ import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query("select posts from Post posts where posts.title like %?1%")
+    @Query("SELECT p FROM Post p WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :title, '%'))")
     Optional<List<Post>> findAllByTitle(String title);
+
+    @Query("SELECT p FROM Post p WHERE " +
+            "LOWER(p.title) LIKE LOWER(CONCAT('%', :title, '%')) " +
+            "AND EXISTS (SELECT t FROM Post p2 JOIN p2.tags t WHERE LOWER(t) IN :tags AND p2.id = p.id)")
+    Optional<List<Post>> findAllByTitleAndTags(@NotBlank @Size(max = 255) String title, List<String> tags);
 }
