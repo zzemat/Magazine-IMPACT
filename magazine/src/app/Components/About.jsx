9b5b19348e4c08df912fc4258625fe9b73fdd827
MouadSadik <sadikmoad90@gmail.com>
2025-03-27 03:46:19 +0100

    fix homepage

--- magazine/src/app/Components/About.jsx
+++ magazine/src/app/Components/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
     <div className='mt-[180vh] md:mt-[90vh]' id='about'>
 
 <div className='flex flex-col justify-center items-center'>
-  <p className='text-black/40 text-sm sm:text-base'>À propos de nous</p>
-  <h1 className='text-[#4F4E4E] font-bold text-2xl sm:text-4xl text-center mx-2'>Handshake infographic mass market</h1>
+  <p className='text-black/40 text-sm sm:text-base'>À propos</p>
+  <h1 className='text-[#4F4E4E] font-bold text-2xl sm:text-4xl text-center mx-2'>Espace pour l’expression, l’échange et l’engagement des étudiants</h1>
 </div>
 
 
@@ -20,10 +20,7 @@ const About = () => {
         <div className='flex flex-col md:w-[40%] hover:scale-110 transition-transform duration-500 cursor-pointer'>
           <Image src={LogoImpact} alt='Logo' height={71} width={163} />
           <h1 className='text-black/40'>À propos de <span className='text-[#EE9F01]'>IMPACT</span></h1>
-          <p className='p-5 bg-neutral-50 text-black/40 mt-5 rounded-sm'>Conversion angel investor entrepreneur first mover advantage.
-            Handshake infographic mass market crowdfunding iteration. Traction stock user experience deployment beta innovator incubator
-            focus. Sales user experience branding growth hacking holy grail monetization conversion prototype stock network effects.
-            Learning curve network effects return on investment bootstrapping business-to-consumer. </p>
+          <p className='p-5 bg-neutral-50 text-black/40 mt-5 rounded-sm'>Club Impact est un club dynamique dédié aux étudiants passionnés par l'interview, le podcast, la photographie et la communication. Ce club offre une plateforme créative où les membres peuvent développer leurs compétences en media, interviewer des personnalités inspirantes, produire des podcasts captivants et capturer des moments uniques à travers la photographie. Club Impact vise à créer un espace de partage, d'apprentissage et de collaboration pour tous ceux qui souhaitent s'impliquer dans le monde de la communication </p>
         </div>
 
         <div className="relative mt-14 group">
@@ -53,10 +50,7 @@ const About = () => {
         <div className='flex flex-col mt-14 md:w-[40%] hover:scale-110 transition-transform duration-500 cursor-pointer'>
           <Image src={MagLogo} alt='Logo' height={71} width={163} />
           <h1 className='text-black/40 '>À propos de <span className='text-[#EE9F01]'>Magazine</span></h1>
-          <p className='p-5 bg-neutral-50 text-black/40 mt-5 rounded-sm'>Conversion angel investor entrepreneur first mover advantage.
-            Handshake infographic mass market crowdfunding iteration. Traction stock user experience deployment beta innovator incubator
-            focus. Sales user experience branding growth hacking holy grail monetization conversion prototype stock network effects.
-            Learning curve network effects return on investment bootstrapping business-to-consumer. </p>
+          <p className='p-5 bg-neutral-50 text-black/40 mt-5 rounded-sm'>Magazine Impact est une plateforme dédiée au partage des idées, des expériences et des hobbies des étudiants. Ce magazine offre un espace où chacun peut exprimer ses passions, découvrir de nouvelles perspectives et s’inspirer des histoires de ses pairs. Que ce soit à travers des articles, des témoignages ou des récits personnels, Magazine Impact vise à créer une communauté engagée, curieuse et ouverte à l’échange. </p>
         </div>
 
 
